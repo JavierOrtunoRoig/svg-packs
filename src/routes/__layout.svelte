b8c94bf53de5df707a91fa4116df9aed9dca9e0b
Javier <javi_roig@hotmail.es>
2022-08-01 07:21:27 +0200

    style: :lipstick: add layout and css to index

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,14 @@
+<slot />
+
+<style>
+  :global(*) {
+    box-sizing: border-box;
+    color: #fff
+  }
+  :global(body) {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+    background: #222;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,3 +10,37 @@
     {/await}
   </div>
 </div>
+
+<style>
+  #container {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    flex-direction: column;
+    gap: 10px;
+  }
+
+  #techs-container {
+    width: 70%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 25px;
+  }
+
+  input {
+    font-family: inherit;
+    font-size: 100%;
+    vertical-align: baseline;
+    border: 0;
+    outline: 0;
+    color: #000;
+    border-radius: 5px;
+    padding: 10px;
+  }
+</style>
