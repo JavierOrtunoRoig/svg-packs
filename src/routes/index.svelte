9788ba7abd963e4a02d3db113a2c5a234dbd8c73
Javier <javi_roig@hotmail.es>
2022-08-01 07:16:32 +0200

    chore: :tada: First commit!

---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<div id="container">
+  <h1>Welcome to a free SVG package</h1>
+  <input bind:value={iconFilter} />
+
+  <div id="techs-container">
+    {#await iconsPromise then icons}
+      {#each icons as {id, slug, title}}
+        <Card {id} {slug} {title}/>
+      {/each}
+    {/await}
+  </div>
+</div>
